629102f0c153d72d31ac89c2d074a2a435706113
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-10-04 14:15:46 +0200

    Change group to 'org.metafacture.contrib'

--- src/main/java/org/metafacture/contrib/csv/SimpleCsvEncoder.java
+++ src/main/java/org/metafacture/contrib/csv/SimpleCsvEncoder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.metafacture.csv;
+package org.metafacture.contrib.csv;
 
 import com.opencsv.CSVWriter;
 import org.metafacture.framework.FluxCommand;
